be649cd5aeada23560247db9cc9254a330c5b35a
Samikshya <write2samikshya@gmail.com>
2021-05-18 03:01:33 +0530

    Add project files.

---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data.OleDb;
+using System.Data.Odbc;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            SqlConnection con = new SqlConnection("data source =.; database=Sample;Integrated Security=SSPI");
+            SqlCommand cmd = new SqlCommand("select * from  Students", con);
+
+            con.Open();
+            SqlDataReader rdr = cmd.ExecuteReader();
+            GridView1.DataSource = rdr;
+            GridView1.DataBind();
+            con.Close();
+
+
+
+
+        }
+
+    }
+}
---
+++ WebApplication2/WebForm1.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace WebApplication2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+            string CS = "data source=.; database=Sample; Integrated Security=SSPI";
+            SqlConnection Con = new SqlConnection(CS);
+            try
+            {
+                SqlCommand cmd = new SqlCommand(" select * from tblemployee", Con);
+                Con.Open();
+                cmd.ExecuteReader();
+                GridView1.DataSource = cmd.ExecuteReader();
+                GridView1.DataBind();
+            }
+
+            catch { }
+            finally
+            {
+                Con.Close();
+            }
+
+        }
+    }
+}
